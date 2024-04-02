5cafe5d8db6366f9846266651709462344a8c283
yoona <103414935+yoonali@users.noreply.github.com>
2024-04-03 04:18:23 +0900

    fix: Peep Dto Name

--- src/modules/auth/dto/peep.user.login.dto.ts
+++ src/modules/auth/dto/peep.user.login.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import PeepUserSaveDto from './peep.user.save.dto.ts';
+import PeepUserSaveDto from './peep.user.save.dto';
 
 export default class PeepUserLoginDto extends PartialType(PeepUserSaveDto) {}
